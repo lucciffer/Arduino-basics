10017bf0446485daf30d7e772ed5c5d303739871
Lucciffer <51166059+lucciffer@users.noreply.github.com>
2020-08-13 19:24:18 +0530

    Update rgb-led-interfacing.ino

--- RGB LED interfacing/rgb-led-interfacing.ino
+++ RGB LED interfacing/rgb-led-interfacing.ino
@@ -1,10 +1,11 @@
-int red_led= 11;
-int green_led = 10;
-int blue_led = 9;
-void setup() {
-  pinMode(red_led, OUTPUT);
-  pinMode(green_led, OUTPUT);
-  pinMode(blue_led, OUTPUT);
+int red_led= 11;  //store 11 value in variable named 'red_led'
+int green_led = 10;  //store 10 value in variable named 'green_led'
+int blue_led = 9; //store 9 value in variable named 'blue_led'
+void setup()  
+{
+  pinMode(red_led, OUTPUT);  //configuring red_led pin (digital pin 11, because red_led has value 11) to OUTPUT
+  pinMode(green_led, OUTPUT); //configuring green_led pin (digital pin 10, because green_led has value 10) to OUTPUT
+  pinMode(blue_led, OUTPUT); //configuring blue_led pin (digital pin 9, because blue_led has value 9) to OUTPUT
 }
 void loop() {
   RGB_color(255, 0, 0); // Red
